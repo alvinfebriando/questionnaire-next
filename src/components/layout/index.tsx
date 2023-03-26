cc1eff3c8205ce94ff5cee360177db5ccd7bdb47
Alvin Febriando <alvinfebriando@outlook.com>
2023-03-26 09:21:35 +0700

    Update header

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,31 @@
+import { Container } from '@mantine/core';
 import { PropsWithChildren } from 'react';
 import Header from './header';
 
 export const Layout = ({ children }: PropsWithChildren) => {
+  const l = {
+    links: [
+      {
+        link: '/report',
+        label: 'Report',
+      },
+      {
+        link: '/survey',
+        label: 'Survey',
+      },
+      {
+        link: '/admin',
+        label: 'Admin',
+      },
+    ],
+  };
+
   return (
     <>
-      <Header />
-      <main>{children}</main>
+      <Header links={l.links} />
+      <Container size='md'>
+        <main>{children}</main>
+      </Container>
     </>
   );
 };
