f708443b41d0d6d9d43367b889124e33c6c6ce53
Alvin Febriando <alvinfebriando@outlook.com>
2023-03-26 19:20:34 +0700

    Fetch surveys to admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,4 +1,30 @@
-const Admin = () => {
-  return <h3>Admin</h3>;
+import { getAllSurvey } from '@/api/survey';
+import { AllSurveyResponse } from '@/api/types';
+import SurveyTable from '@/components/survey/surveyTable';
+import { Box, Button, Stack } from '@mantine/core';
+import { IconPlus } from '@tabler/icons-react';
+import { MouseEventHandler } from 'react';
+
+const createNewSurvey: MouseEventHandler<HTMLButtonElement> = e => {
+  console.log(e.currentTarget);
 };
+
+const Admin = (surveys: AllSurveyResponse) => {
+  console.log(surveys);
+  return (
+    <Stack>
+      <Box>
+        <Button leftIcon={<IconPlus />} onClick={createNewSurvey}>
+          New
+        </Button>
+      </Box>
+      <SurveyTable survey={surveys} role='Admin' />
+    </Stack>
+  );
+};
+
+export async function getStaticProps() {
+  const surveys = await getAllSurvey();
+  return { props: surveys };
+}
 export default Admin;
