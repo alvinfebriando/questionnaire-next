cf9287493ca0c03c9e6adcfbb1b4fdea94778e70
Alvin Febriando <alvinfebriando@outlook.com>
2023-04-07 13:33:36 +0700

    Redirect home page to /survey

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 
 const Home: NextPage = () => {
   return <h1>Home</h1>;
+};
+
+export const getStaticProps: GetStaticProps = ctx => {
+  return {
+    redirect: {
+      destination: '/survey',
+      permanent: true,
+    },
+  };
 };
 
 export default Home;
