10a49e27c57fd31d794801521a72a579245a7944
Alvin Febriando <alvinfebriando@outlook.com>
2023-04-11 17:05:51 +0700

    Add tooltip

--- src/components/survey/respondentActionRow.tsx
+++ src/components/survey/respondentActionRow.tsx
@@ -1,5 +1,5 @@
 import { SurveyTableRecords } from '@/types/survey';
-import { ActionIcon, Group } from '@mantine/core';
+import { ActionIcon, Group, Tooltip } from '@mantine/core';
 import { IconClipboard } from '@tabler/icons-react';
 import { useRouter } from 'next/router';
 
@@ -11,12 +11,14 @@ const RespondentActionRow = ({ survey }: RespondentActionRowProps) => {
   const router = useRouter();
   return (
     <Group spacing={4} position='right' noWrap>
-      <ActionIcon
-        color='green'
-        onClick={() => router.push(`/survey/${survey.id}`)}
-      >
-        <IconClipboard size={16} />
-      </ActionIcon>
+      <Tooltip label='Answer'>
+        <ActionIcon
+          color='green'
+          onClick={() => router.push(`/survey/${survey.id}`)}
+        >
+          <IconClipboard size={16} />
+        </ActionIcon>
+      </Tooltip>
     </Group>
   );
 };
